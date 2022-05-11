0a49ddb76a680eadd29e433260baf567edd15672
IvarLaM <62998604+IvarLaM@users.noreply.github.com>
2022-05-11 08:51:47 +0200

    Update mail.sh

--- mail.sh
+++ mail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dossier=/home/lucho/Documents/clubvercors/horaires-`date +%Y-%m`
+dossier=/home/$USER/horaires-`date +%Y-%m`
 zip $dossier
 swaks -t <mail of receiver> -s smtp.server:port -tls -au <Auth_Utilisateur> -ap <Auth_Pass> -f <email_sender> --h-Subject "Your_Subject" --attach /home/lucho/Documents/clubvercors/horaires-`date +%Y-%m`.zip
