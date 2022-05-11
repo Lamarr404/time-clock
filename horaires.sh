efc49bd24dba83c591d23695769fc9ecb3ff5a25
IvarLaM <62998604+IvarLaM@users.noreply.github.com>
2022-05-11 08:53:06 +0200

    Update horaires.sh

--- horaires.sh
+++ horaires.sh
@@ -5,20 +5,20 @@
 ARGUMENTS=$1
 WEEK=$2
 ROAD=$3
-CHEMIN=/home/lucho/Documents/clubvercors/horaires-2022-05/horaires-2022-05-$ROAD
+CHEMIN=/home/$USER/horaires-2022-05/horaires-2022-05-$ROAD
 DAY=$4
 
 case $1 in
 	-n | --new) 
 
-		mkdir -p /home/lucho/Documents/clubvercors/horaires-`date +%Y-%m`
-		echo 'Lundi: Mardi: Mercredi: Jeudi: Vendredi: Samedi: Dimanche:' | tr " " "\n" > /home/lucho/Documents/clubvercors/horaires-`date +%Y-%m`/horaires-`date +%Y-%m`-1
+		mkdir -p /home/$USER/horaires-`date +%Y-%m`
+		echo 'Lundi: Mardi: Mercredi: Jeudi: Vendredi: Samedi: Dimanche:' | tr " " "\n" > /home/$USER/horaires-`date +%Y-%m`/horaires-`date +%Y-%m`-1
 
 		;;
 
 	-w | --new-week)
 
-		new_week=$(echo 'Lundi: Mardi: Mercredi: Jeudi: Vendredi: Samedi: Dimanche:' | tr " " "\n" > /home/lucho/Documents/clubvercors/horaires-`date +%Y-%m`/horaires-`date +%Y-%m`-$WEEK)
+		new_week=$(echo 'Lundi: Mardi: Mercredi: Jeudi: Vendredi: Samedi: Dimanche:' | tr " " "\n" > /home/$USER/horaires-`date +%Y-%m`/horaires-`date +%Y-%m`-$WEEK)
 
 		;;
 
